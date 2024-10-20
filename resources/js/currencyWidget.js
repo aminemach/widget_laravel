019f601838b8fdc900abaf73704d4dc08f872369
MohamedAmineMACHFER <amine.machfer1@gmail.com>
2024-10-20 01:26:15 +0100

    Initial commit

--- resources/js/currencyWidget.js
+++ resources/js/currencyWidget.js
@@ -3,13 +3,24 @@
     const redirectUrl = scriptTag.getAttribute('data-redirect-url');
     const btnActionClass = scriptTag.getAttribute('data-btn-action');
     const stickyIcon = document.createElement('div');
+
+    if (window.widgetSDK) {
+        window.widgetSDK.run({
+            websiteToken: websiteToken,
+            baseUrl: baseUrl,
+            widgetColor: widgetColor,
+            headerColor: headerColor,
+            buttonColor: buttonColor
+        });
+    }
+
     stickyIcon.id = 'sticky-icon';
     stickyIcon.style.position = 'fixed';
     stickyIcon.style.bottom = '50%';
     stickyIcon.style.left = '0';
     stickyIcon.style.width = '50px';
     stickyIcon.style.height = '50px';
-    stickyIcon.style.backgroundColor = '#FFFFFF'; 
+    stickyIcon.style.backgroundColor = widgetColor; 
     stickyIcon.style.color = 'white';
     stickyIcon.style.display = 'flex';
     stickyIcon.style.alignItems = 'center';
@@ -18,10 +29,11 @@
     stickyIcon.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.3)';
     stickyIcon.style.zIndex = '1000';
     stickyIcon.innerHTML = '<span style="font-size: 24px;">💱</span>';
-    stickyIcon.style.borderTopLeftRadius = '0'; 
-    stickyIcon.style.borderBottomLeftRadius = '0'; 
-    stickyIcon.style.borderTopRightRadius = '50%'; 
-    stickyIcon.style.borderBottomRightRadius = '50%'; 
+    stickyIcon.style.borderTopLeftRadius = '0';
+    stickyIcon.style.borderBottomLeftRadius = '0';
+    stickyIcon.style.borderTopRightRadius = '50%';
+    stickyIcon.style.borderBottomRightRadius = '50%';
+
 
     document.body.appendChild(stickyIcon);
     stickyIcon.addEventListener('click', function() {
